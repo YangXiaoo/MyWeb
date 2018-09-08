9ecaa2b21c7e9625fbe861ebaf9ec0972fd973f6
YangXiaoo <33798487+YangXiaoo@users.noreply.github.com>
2018-09-08 21:01:01 +0800

    delete

--- js技巧.js
+++
@@ -1,4 +0,0 @@
-
-//====================== js技巧   ===============================//
-onmouseover="this.style.backgroundColor='#deebef'";
-onmouseout="this.style.backgroundColor='';
--- 小技巧.php
+++
@@ -1,9 +0,0 @@
-<?php
-//================================     smarty   ======================================//
-//smarty分为模板文件和Php文件
-$smarty->assign("showmusic_false","F");//将showmusic_false替换为F
-$smarty->display("listen_music.tpl");//展示内容
-//================================  END  ====================================//
-
-onmouseover="this.style.backgroundColor='#deebef'"
-
